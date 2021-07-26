923c895f53d4426111ce98aa2cf57099043d4ba1
dsaswal from ec2 <ec2-user@ip-172-31-33-237.us-east-2.compute.internal>
2021-07-26 00:07:45 +0000

    CRUD operations working

--- src/main/java/dsa/selfprojects/notesapp/rest/NoteRestController.java
+++ src/main/java/dsa/selfprojects/notesapp/rest/NoteRestController.java
@@ -1,4 +1,4 @@
-package dsa.selfprojects.notesapp.dao;
+package dsa.selfprojects.notesapp.rest;
 
 import java.util.List;
 
@@ -7,22 +7,48 @@ import org.springframework.web.bind.annotation.*;
 
 
 import dsa.selfprojects.notesapp.entity.Note;
-import dsa.selfprojects.notesapp.dao.NoteDAO;
+import dsa.selfprojects.notesapp.service.NoteService;
 
 @RestController
 @RequestMapping("/api")
 public class NoteRestController {
-	private NoteDAO noteDAO;
+	private NoteService noteService;
 
 	@Autowired
-	public NoteRestController(NoteDAO theNoteDAO) {
-		noteDAO = theNoteDAO;
+	public NoteRestController(NoteService theNoteService) {
+		noteService = theNoteService;
 	}
 
 	@GetMapping("/notes")
 	public List<Note> getNotes() {
-		return noteDAO.getNotes();
+		return noteService.getNotes();
 	}
+
+	@GetMapping("/notes/{noteId}")
+	public Note getNote(@PathVariable long noteId) {
+		return noteService.getNote(noteId);
+	}
+
+        @PostMapping("/notes")
+        public Note saveNote(@RequestBody Note theNote) {
+                theNote.setId(0);
+		noteService.saveNote(theNote);
+		return theNote;
+        }
+
+        @PutMapping("/notes")
+        public Note updateNote(@RequestBody Note theNote) {
+		noteService.updateNote(theNote);
+		return theNote;
+        }
+
+        @DeleteMapping("/notes/{noteId}")
+        public String updateNote(@PathVariable long noteId) {
+		//todo - validate if note exists
+			
+		noteService.deleteNote(noteId);
+		return "Note deleted successfully, note id - " + noteId;
+        }
 
 }
 
